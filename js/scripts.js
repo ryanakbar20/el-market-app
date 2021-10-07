c5d418e9f025a819055085ddd296604f177bd18c
EL-markt-Arda <afidatsanaarda@gmail.com>
2021-10-07 13:16:27 +0700

    ini pesan commit nya

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+async function fetchAsync(user) {
+    let rensponse = await fetch('https://api.github.com/users/EL-markt-Arda')
+    rensponse = await rensponse.json();
+
+    console.log(rensponse);
+
+    document.getElementById("EL-markt-Arda").innerHTML = rensponse.login;
+
+    const image = document.getElementById("")
+}
